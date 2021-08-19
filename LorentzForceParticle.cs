f8d18a0a06d8261ce6d5fae29938ee4b8de79061
Banu Darius Matei <30901594+TheNumber5@users.noreply.github.com>
2021-08-19 13:05:15 +0300

    Add files via upload

--- LorentzForceParticle.cs
+++ LorentzForceParticle.cs
@@ -5,14 +5,15 @@ using UnityEngine;
 public class LorentzForceParticle : MonoBehaviour {
 	private Rigidbody particleRigidbody;
 	public float charge, electricField, magneticField;
-	public float x, y, z;
+	public float xM, yM, zM;
+	public float xE, yE, zE;
 	void Awake() {
 		particleRigidbody = this.gameObject.GetComponent<Rigidbody>();
 		ParticleManager.instance.particleList.Add(this);
 	}
 	void FixedUpdate() {
-		Vector3 magneticVector = new Vector3(x*magneticField, y*magneticField, z*magneticField);
-		Vector3 lorentzForce = Vector3.forward*charge*electricField+charge*Vector3.Cross(particleRigidbody.velocity, magneticVector);
+		Vector3 magneticVector = new Vector3(xM*magneticField, yM*magneticField, zM*magneticField);
+		Vector3 lorentzForce = new Vector3(xE, yE, zE)*charge*electricField+charge*Vector3.Cross(particleRigidbody.velocity, magneticVector);
 		particleRigidbody.AddForce(lorentzForce*Time.deltaTime, ForceMode.Impulse);
 	}
 	public void Eliminate() {
